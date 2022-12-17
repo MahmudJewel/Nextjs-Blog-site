407d159afc37126908267c6a20135991eeea03d8
Mahmud <jmbengal.mbstu@gmail.com>
2022-12-17 17:44:00 +0600

    demo blog aded

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import { NextPage } from "next";
+import { use } from "react";
+import { getBlogFileNames } from "../lib/blogs";
+// import { getBlogFileNames } from "../lib/blogs";
+
+async function getBlogs() {
+  const fileName = getBlogFileNames();
+  return fileName;
+}
+
 
 const Page: NextPage = () => {
+  const names = use(getBlogs());
   return (
     <div>
       Hello World!!!
       <h1 className="text-3xl font-bold underline">Hello World</h1>
+      {JSON.stringify(names)}
+      {/* {names} */}
     </div>
   );
 };
 
 export default Page;
-
