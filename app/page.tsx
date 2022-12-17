5e4fba0fce995dcbef4c6cafde90d567a1f0f419
Mahmud <jmbengal.mbstu@gmail.com>
2022-12-17 15:17:40 +0600

    typeScript added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from "next";
 
-const Page:NextPage = () => {
-    return (
-        <div>
-            Hello World!!!
-            <h1 className="text-3xl font-bold underline">Hello World</h1>
-        </div>
-    )
-}
+const Page: NextPage = () => {
+  return (
+    <div>
+      Hello World!!!
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  );
+};
 
 export default Page;
+
